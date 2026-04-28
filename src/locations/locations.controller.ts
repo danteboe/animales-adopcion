ef0229489d4de45ea6b40510a34a279b41ccf50f
Juan-David <jd.torresa1@uniandes.edu.co>
2026-04-28 16:29:41 -0500

    Initial commit: NestJS Animal Adoption API with PostgreSQL integration

---
+++ src/locations/locations.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { CreateLocationDto } from './dto/create-location.dto';
+import { UpdateLocationDto } from './dto/update-location.dto';
+import { LocationsService } from './locations.service';
+
+@Controller('locations')
+export class LocationsController {
+  constructor(private readonly locationsService: LocationsService) {}
+
+  @Post() create(@Body() dto: CreateLocationDto) {
+    return this.locationsService.create(dto);
+  }
+  @Get() findAll() {
+    return this.locationsService.findAll();
+  }
+  @Get(':id') findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.locationsService.findOne(id);
+  }
+  @Patch(':id') update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateLocationDto,
+  ) {
+    return this.locationsService.update(id, dto);
+  }
+  @Delete(':id') remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.locationsService.remove(id);
+  }
+}
